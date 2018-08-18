8d32c90086484b73b5479b8395cd7ced1f967345
zyx1996 <34615338+zyx1996@users.noreply.github.com>
2018-08-18 10:35:40 +0800

    Update HW1_15.cpp

--- HW1_15.cpp
+++ HW1_15.cpp
@@ -5,12 +5,31 @@
 using namespace std;
 
 string file="E:\\迅雷下载\\hw1_15_train.dat";//读取文件名
-#define demension 5   //特征维度+x0
-vector<double> weight(demension);
-void getdata(ifstream &datafile)
-{[InternetShortcut]
-URL=https://github.com/zyx1996/homework
 
+#define demension 5   //特征维度+x0
+
+double weight[demension];//权重
+//训练样本结构
+struct record{
+  double input[demension];
+  int output;
+};
+//训练样本向量
+vector<record> trainingset;
+//把数据读取到训练样本向量
+void getdata(ifstream &datafile){
+  while(!datafile.fail()){
+    for(int i=i;i<demension;i++){
+      datafile>>currecord.input[i];
+    }
+    datafile>>currecord.output[i];
+    trainingset.push_back(currecord);
+  }
+  datafile.close();
+  n=trainingset.size();
+}
+
+void pla(){
   
 }
 void main()
@@ -24,7 +43,6 @@ void main()
     exit(1);
     system("pause");
   }
-  int i;
-  for(auto x:weight)
+  for(auto x:weight)x=0;
   pla();
 }
